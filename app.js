3328fb75408671ec79c3ddee371ee2f8347d2226
Jon Flores <jflore61@ucsc.edu>
2021-09-13 18:52:21 +0200

    database linked

--- app.js
+++ app.js
@@ -5,18 +5,22 @@ require("dotenv/config");
 // ℹ️ Connects to the database
 require("./db");
 
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
-const express = require("express");
-
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
+const express = require('express');
 const hbs = require("hbs");
-
 const app = express();
-
+const axios = require('axios');
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
+
+// setting the TMDB-api goes here:
+const TMDBApi_key = process.env.API_Key;
+ 
+// const TMDBApi = new TMDBApi({
+//   API_Key: process.env.API_Key,
+//   });
+// const accessToken = spotifyApi.clientCredentialsGrant()
+//   .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+//   .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // default value for title local
 const projectName = "project2-moviesList";
@@ -27,7 +31,27 @@ app.locals.title = `${capitalized(projectName)} created with IronLauncher`;
 // 👇 Start handling routes here
 const index = require("./routes/index");
 app.use("/", index);
+// const movies = require("./routes/movies");
+// app.use("/", movies);
 
+app.get('/movie-search', (req, res) => {
+  const movie = req.query.movie;
+  const movieQuery = movie.split(' ').join('+');
+  console.log(movieQuery);
+  
+  axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${TMDBApi_key}&query=${movieQuery}`)
+    
+    .then((response) => {
+      console.log('The response from the API: ',response.body);
+    
+
+     // res.render('artist-search-results', { data });
+    })
+
+    .catch((err) =>
+      console.log('The error while searching artists occurred: ', err)
+    );
+});
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
---
+++ models/Movies.js
@@ -0,0 +1,7 @@
+const movieSchema = new Schema({
+    Schema = new Schema({
+        images = Object,
+        base_url = String,
+        secure_base_url = String,
+        
+    })
