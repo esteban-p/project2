75789b7f0e316654191ad3682ef86dff7fea002f
Esteban <esteban.github@gmail.com>
2021-09-16 16:20:45 +0200

    change MONGODB_URI

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ require("./config")(app);
 
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
-//const DB_URL = process.env.MONGODB_URI;
-const DB_URL = process.env.MONGO;
+const DB_URL = process.env.MONGODB_URI;
 
 app.use(
     session({
